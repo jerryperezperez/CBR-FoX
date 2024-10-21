b8c4815ba66aa05ed51d3403b5b7bdd7e3a9c879
Gerardo Pérez Pérez <jerryperezxbox@gmail.com>
2024-10-21 12:59:24 -0600

    Se modifica el __init__ de cbr_fox_factory para evitar el envío estricto del diccionario de configuración.

--- factory/cbr_fox_factory.py
+++ factory/cbr_fox_factory.py
@@ -4,7 +4,17 @@ from cbr_fox import cbr_fox
 class cbr_fox_factory:
     def __init__(self, techniques):
         # Store techniques as a dictionary, where the key is the technique name and the value is the cbr_fox object
-        self.techniques_dict = {technique: cbr_fox(technique, **params) for technique, params in techniques}
+        self.techniques_dict = dict()
+        for item in techniques:
+            if isinstance(item, tuple) and len(item) == 2:
+                name, config = item
+                self.techniques_dict[name] = cbr_fox(name, **config)
+            else:
+                self.techniques_dict[item] = cbr_fox(item)
+    
+    def explan_all_techniques(self,  training_windows, target_training_windows, forecasted_window, prediction, num_cases):
+        for technique in self.techniques_dict:
+            technique.explain(training_windows, target_training_windows, forecasted_window, prediction, num_cases)
 
     # Override __getitem__ to allow dictionary-like access
     def __getitem__(self, technique_name):
